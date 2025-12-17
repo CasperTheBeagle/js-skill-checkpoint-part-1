d26b45090d53d3e245aec336cf0b56633df64d8e
CasperTheBeagle <146039265+CasperTheBeagle@users.noreply.github.com>
2025-12-17 13:46:54 +0700

    feat:finish Q#1

--- question-1.js
+++ question-1.js
@@ -6,3 +6,26 @@ const myTodo = [
   { id: 4, todo: "Wash dishes" },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+// 1) เพิ่ม Object ใหม่เข้าไปใน myTodo ซึ่งมี id เป็น 5 และมี todo เป็น "Walk the dog" 
+myTodo.push({id:5,todo:"walk the dog"});
+
+
+// 2) แก้ไขชื่อ Object ที่มี id เป็น 4 เพื่อให้ todo ของ Object นั้นมีค่าเป็น "Go to the gym"
+myTodo[3].todo="Go to The Gym";
+
+// 3) ลบ Object สุดท้ายใน Array ออก
+// console.log(myTodo);
+myTodo.pop();
+
+
+// 4) นำ myTodo มาแสดงบนหน้าจอ Console
+console.log(myTodo);
+
+// 5) ให้นำข้อมูลใน Object สุดท้ายมาแสดงบนหน้าจอ Console ในรูปแบบ
+// "To-do id: <ค่าของ id>, <ค่าของ todo>" (ห้าม Hard-Code ค่าเข้าไปใน String โดยตรง)
+const lastMyTodo = myTodo[myTodo.length-1];
+console.log(`To-do id: ${lastMyTodo.id}, ${lastMyTodo.todo}`);
+
+
+
