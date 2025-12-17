c96ab115f8c93980d519293e04927df9bb92dbe9
CasperTheBeagle <146039265+CasperTheBeagle@users.noreply.github.com>
2025-12-17 15:03:21 +0700

    feat: finish Q#5

--- question-5.js
+++ question-5.js
@@ -17,13 +17,29 @@ const products = [
   },
 ];
 
-const promotionCode = "";
+// const promotionCode = "";
+// const promotionCode = "SALE20";
+const promotionCode = "SALE50";
 // เริ่มเขียนโค้ดตรงนี้
 
-//ให้เขียนฟังก์ชันชื่อ calculateTotalPrice ที่รับ products และ promotionCode เป็น Argument  
-function calculateTotalPrice(products,promotionCode){
-  let sumProductPrice;
-  let salePercentStatus ={"SALE20","SALE50"};
+let productsInCart = products;
+let discountCode = promotionCode;
 
-  return 0;
+//ให้เขียนฟังก์ชันชื่อ calculateTotalPrice ที่รับ products และ promotionCode เป็น Argument  
+function calculateTotalPrice(productsInCart, discountCode) {
+  let sumProductPrice=0;
+  for (let i = 0; i < products.length; i++) {
+    sumProductPrice += products[i].price * products[i].quantity;
+  }
+  if (promotionCode === "SALE20") { // "SALE20"
+    sumProductPrice = sumProductPrice * ((100-20)/100); // ลด 20% จากมูลค่ารวมของสินค้าในตะกร้า
+
+  } else if (promotionCode === "SALE50") { // "SALE50"
+    sumProductPrice = sumProductPrice * ((100-50)/100); // ลด 50% จากมูลค่ารวมของสินค้าในตะกร้า
+  }
+  else {
+    sumProductPrice = sumProductPrice; // ไม่ได้รับส่วนลด
+  }
+  return sumProductPrice;
 }
+console.log(calculateTotalPrice(products, promotionCode));
